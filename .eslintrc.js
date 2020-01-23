5de8479351e48c79dc3d3ecffd14b70b6a1be31c
Patrick <trickstival@gmail.com>
2020-01-23 13:57:29 -0300

    Added eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: [
+        // add more generic rulesets here, such as:
+        'eslint:recommended',
+        'plugin:vue/recommended'
+    ],
+    rules: {
+        // override/add rules settings here, such as:
+        // 'vue/no-unused-vars': 'error'
+    },
+    plugins: ['jest'],
+    env: {
+        "jest/globals": true
+    }
+}
