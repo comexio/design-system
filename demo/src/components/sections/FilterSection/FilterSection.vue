696649c44f11d2b27054bc6e2f5d5303f7353b16
rafael <rafael.bastiansch@logcomex.com>
2020-02-07 17:49:25 -0300

    Adicionado labeledChip ao design system

---
+++ demo/src/components/sections/FilterSection/FilterSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    Filters
+    <labeled-chip
+      :item="item"
+    />
+  </div>
+</template>
+
+<script>
+import { LabeledChip } from '@/../../src'
+export default {
+  components: {
+    LabeledChip
+  },
+  data () {
+    return {
+      item: {
+        text: 'Últimos 3 meses',
+        number: '56',
+        filter: {
+          id: 'period'
+        }
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/filters/index.js
@@ -0,0 +1,5 @@
+import LabeledChip from './LabeledChip'
+
+export {
+    LabeledChip
+}
