2f14bf59f16d15e9aed92e2a8ae6229a87c0ce04
rafael <rafael.bastiansch@logcomex.com>
2020-06-04 18:41:30 -0300

    Adicionado card padrão do sistema

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 export * from './components/inputs'
 export * from './components/filters'
+export * from './components/cards'
 
 import * as inputs from './components/inputs'
 import * as filters from './components/filters'
+import * as cards from './components/cards'
 
 const components = {
     ...inputs,
-    ...filters
+    ...filters,
+    ...cards
 }
 
 export function install (Vue) {
