97c8fae7eb49de6b1040e47f881a0dae7fe76a8c
rafael <rafael.bastiansch@logcomex.com>
2020-08-19 14:39:53 -0300

    (LOG-8028) - Add arrow effect to lines

--- src/components/charts/LRouteChartLine.vue
+++ src/components/charts/LRouteChartLine.vue
@@ -40,6 +40,25 @@ export default {
 }
 
 .LRouteChart__line__item--text {
-  box-shadow: inset 2px 0px 0px 0px white;
+  &:nth-child(-n+2) {
+    box-shadow: inset 2px 0px 0px 0px white;
+  }
+  position: relative;
 }
+
+.LRouteChart__line__item {
+  &:not(:nth-child(-n+2)) {
+    &::before {
+      content: "";
+      left: -30px;
+      position: absolute;
+      top: 4px;
+      padding: 12px;
+      border-top: 2px solid white;
+      border-right: 2px solid white;
+      transform: rotate(45deg);
+    }
+  }
+}
+
 </style>
