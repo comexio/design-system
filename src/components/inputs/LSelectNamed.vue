20eaae5b787330fac9b75fb881019a4ffdc7e2d9
rafael <rafael.bastiansch@logcomex.com>
2020-09-03 17:29:08 -0300

    (LOG-7790) - Adicionado select named

---
+++ src/components/inputs/LSelectNamed.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-select
+    v-model="inputValue"
+    v-bind="$attrs"
+    :items="items"
+    class="LSelectNamed__select"
+    item-value="value"
+    item-text="text"
+    :menu-props="{ bottom: true, offsetY: true }"
+    auto
+    v-on="$listeners"
+  />
+</template>
+
+<script>
+export default {
+  name: 'LSelectNamed',
+  props: {
+    value: {
+      type: [String, Number],
+      default: null
+    },
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    inputValue: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.LSelectNamed__select {
+  font-size: 13px;
+  height: 30px !important;
+  max-width: 220px !important;
+  padding: 0px !important;
+  margin-top: -4px !important;
+
+  ::v-deep {
+    .v-select__selections::before {
+      content: "Mostrando por:\00a0";
+      color: $martinique;
+    }
+
+    .v-input__icon .v-icon::before {
+      content: "\F0140";
+      color: $wisteria;
+    }
+
+    .v-input__slot::before {
+      border-color: $wisteria !important;
+    }
+
+    .v-select__selection {
+      color: $martinique;
+    }
+  }
+}
+</style>
---
+++ test/components/inputs/lSelectNamed.spec.ts
@@ -0,0 +1,64 @@
+import { mount, Wrapper } from '@vue/test-utils'
+import { initSetupComponent, addElemWithDataAppToBody } from '~/test/utils.setup'
+import LSelectNamed from '~/src/components/inputs/LSelectNamed.vue'
+
+const setupDefault = initSetupComponent()
+const defaultParams = {
+  sync: false,
+  ...setupDefault
+}
+
+const fakeItems = [{
+  value: 'one',
+  text: 'One'
+}, {
+  value: 'two',
+  text: 'Two'
+}, {
+  value: 'three',
+  text: 'Three'
+}]
+
+describe('selectNamed component', () => {
+  addElemWithDataAppToBody()
+  let selectNamed: Wrapper<LSelectNamed>
+
+  beforeAll(() => {
+    selectNamed = mount(LSelectNamed, {
+      ...defaultParams,
+    })
+  })
+
+  it('render component without data', () => {
+    expect(selectNamed.exists()).toBe(true)
+  })
+
+  it('click to open list', async () => {
+    const input = () => selectNamed.find('.v-input__slot')
+    const selectList = () => selectNamed.find('.v-select-list')
+    input().trigger('click')
+
+    await selectNamed.vm.$nextTick()
+
+    expect(selectList().exists()).toBe(true)
+  })
+
+  it('render items', async () => {
+    selectNamed.setProps({ items: fakeItems })
+
+    await selectNamed.vm.$nextTick()
+
+    const listItems = () => selectNamed.findAll('.v-list-item')
+    expect(listItems().length).toBe(3)
+  })
+
+  it('select item', async () => {
+    const listItems = () => selectNamed.findAll('.v-list-item')
+    const firstItem = () => listItems().at(0)
+    firstItem().trigger('click')
+
+    await selectNamed.vm.$nextTick()
+
+    expect(selectNamed.emitted().input).toEqual([["one"]])
+  })
+})
