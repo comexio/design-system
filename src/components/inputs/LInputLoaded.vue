55bf834000be29cf818f88c0d7899e1caabaf5c8
rafael <rafael.bastiansch@logcomex.com>
2020-11-05 18:37:36 -0300

    (LOG-11196) - Adicionado parametro para não adicionar item novo

--- src/components/inputs/LInputLoaded.vue
+++ src/components/inputs/LInputLoaded.vue
@@ -42,7 +42,8 @@ export default {
     items: {
       type: Array,
       default: () => ([])
-    }
+    },
+    removeTypedFilters: Boolean
   },
   data () {
     return {
@@ -77,8 +78,20 @@ export default {
       const { field } = this
       this.$emit('getItems', { field })
     },
-    changeFilter () {
+    changeFilter (filters) {
+      if (this.removeTypedFilters) {
+        this.filterItems(filters)
+      }
       this.searchInput = null
+    },
+    filterItems (filters) {
+      const filteredOptions = filters.filter(item => {
+        if (item && item.text) {
+          return item
+        }
+      })
+
+      this.selectedOptions = filteredOptions
     }
   }
 }
