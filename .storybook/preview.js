6550e0def077f821e280b3a72f0805a197945a5a
Kauan Schaeffer <kauan.schaeffer@logcomex.com>
2020-12-22 17:07:03 -0300

    (LOG-13993) Aplicando vuetify nas stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,11 @@
+import { addDecorator } from '@storybook/vue';
+import vuetify from './vuetify_storybook';     
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+addDecorator(() => ({
+  vuetify,
+  template: '<v-app><story/></v-app>'
+}))
