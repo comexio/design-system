441216bc3bf05e2c3e264b8aade00ee1eed79b23
Kauan Schaeffer <kauan.schaeffer@logcomex.com>
2020-12-22 13:28:08 -0300

    (LOG-13993) Criando esqueleto do Storybook
    - Adição da dependẽncia
    - Configurações para habilitar sass

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+    return config;
+  },
+  "stories": [
+    "../docs/**/*.stories.mdx",
+    "../docs/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
