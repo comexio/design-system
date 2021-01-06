d3512362a1e2a65ac41fd6caa501e336f1fa1629
gustavoraamos <gustavo.ramos@logcomex.com>
2021-01-06 16:29:09 -0300

    (LOG-12926) - Desenvolver clique para abrir modal no grafico
    - Documentação
    
    #feature
    #time 1m
    #comment
    - Documentação

--- docs/stories/components/charts/LLinearChartExpand.stories.js
+++ docs/stories/components/charts/LLinearChartExpand.stories.js
@@ -10,7 +10,7 @@ export default {
     loading: {control: 'boolean', description: 'Expanded chart loading: true / not loading: false'},
     expand: {action: 'expand', description: 'Toggle chart expanded when "Recolher" clicked'},
     showDetails: {action: 'showDetails', description: 'Emmit custom event passing a value to show details'},
-    search: {actions: 'search', table: {disable: true}},
+    search: {action: 'search', table: {disable: true}},
     showDetailsIcon: {table: {disable: true}},
   },
 };
