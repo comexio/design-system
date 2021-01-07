adfb4522fff612a38d0c73aa8df249f6569e694c
matheus <matheusdasilvarocha@gmail.com>
2021-01-07 14:52:54 -0300

    Documentação do LModal

---
+++ docs/stories/components/modals/LModal.stories.js
@@ -0,0 +1,50 @@
+// import LModal from '@components/modal/LModal.vue';
+
+// export default {
+//   title: 'Design System/Components/Base/Modal/LModal',
+//   component: LModal,
+//   argTypes: {
+//     dialog: { control: 'boolean', description:'Open and close modal open: true / close: false' },
+//     textCancel: { control: 'text', description: 'Text Button Cancel' },
+//     textConfirm: { control: 'text', description: 'Text Button Confirm' },
+//     disabledConfirm: { control: 'boolean', description:'Disable button confirm disable: true / enable: false' },
+//     confirmButton: { control: 'boolean', description:'Show button confirm show: true / show: false' },
+//     clearButton: { table: { disable: true }, description:'N' },
+//     textClear: { control: 'text', description: 'Text button clear' },
+//     clearIcon: { control: 'text', description: 'Icon button clear' },
+//     loading: { control: 'boolean', description: 'Loading modal'},
+//     close: { action: 'close', description: 'Event close modal' },
+//     confirm: { action: 'confirm', description: 'Event confirm modal' },
+//     clear: { action: 'clear', description: 'Event clear modal' },
+//   },
+// };
+
+// const Template = (args, { argTypes }) => ({
+//   props: Object.keys(argTypes),
+//   components: { LModal },
+//   template: '<l-modal v-bind="$props" @close="close" @confirm="confirm" @clear="clear" style="height: 400px" ></l-modal>'
+// });
+
+// export const Default = Template.bind({});
+// Default.args = {
+//   dialog: true,
+//   disabledConfirm:true,
+//   textConfirm: 'Confirmar',
+//   textCancel: 'Cancelar',
+// };
+
+// export const Clear = Template.bind({});
+// Clear.args = {
+//   dialog: true,
+//   textClear: 'Button clear',
+//   textConfirm: 'Confirmar',
+//   textCancel: 'Cancelar',
+// };
+
+// export const Loading = Template.bind({});
+// Loading.args = {
+//   dialog: true,
+//   textConfirm: 'Confirmar',
+//   textCancel: 'Cancelar',
+//   loading: true
+// };
