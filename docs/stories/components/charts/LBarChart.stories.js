89ccf1bcafb14058f97a7b226c65667e2f3caf14
MatheuzsrLog <matheus.rocha@logcomex.com>
2021-01-12 10:24:53 -0300

    Alterar estrutura de exibicao

--- docs/stories/components/charts/LBarChart.stories.js
+++ docs/stories/components/charts/LBarChart.stories.js
@@ -1,47 +1,56 @@
-import LBarChart from '@components/charts/LBarChart.vue';
+import LBarChart from "@components/charts/LBarChart.vue";
 
 export default {
-  title: 'Design System/Components/Charts/Secondary Bar',
+  title: "Components/Charts/Secondary Bar",
   component: LBarChart,
   argTypes: {
-    data: {control: 'object', description: 'Chart data object: title, description, quantity, total, percentage'},
-    color: { control: 'color', description: 'Chart color'},
-    maxQuantity: {table: {disable: true}},
-    othersLabel: {table: {disable: true}},
-    toggleLast: {table: {disable: true}},
-    hasTitle: {table: {disable: true}},
-  },
+    data: {
+      control: "object",
+      description:
+        "Chart data object: title, description, quantity, total, percentage"
+    },
+    color: { control: "color", description: "Chart color" },
+    maxQuantity: { table: { disable: true } },
+    othersLabel: { table: { disable: true } },
+    toggleLast: { table: { disable: true } },
+    hasTitle: { table: { disable: true } }
+  }
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { LBarChart },
-  template: '<l-bar-chart v-bind="$props"> </l-bar-chart>',
+  template: '<l-bar-chart v-bind="$props"> </l-bar-chart>'
 });
 
 export const Default = Template.bind({});
 Default.args = {
-  data: [{
-    title: "0001",
-    description: "Description ...........................................................",
-    quantity: "100",
-    total: "50.000",
-    percentage: 50
-  },
-  {
-    title: "0002",
-    description: "Description ...........................................................",
-    quantity: "100",
-    total: "30.000",
-    percentage: 30
-  },
-  {
-    title: "0003",
-    description: "Description ...........................................................",
-    quantity: "100",
-    total: "20.000",
-    percentage: 20
-  },],
-  color: '#D4C5EB',
-  hasTitle: true,
+  data: [
+    {
+      title: "0001",
+      description:
+        "Description ...........................................................",
+      quantity: "100",
+      total: "50.000",
+      percentage: 50
+    },
+    {
+      title: "0002",
+      description:
+        "Description ...........................................................",
+      quantity: "100",
+      total: "30.000",
+      percentage: 30
+    },
+    {
+      title: "0003",
+      description:
+        "Description ...........................................................",
+      quantity: "100",
+      total: "20.000",
+      percentage: 20
+    }
+  ],
+  color: "#D4C5EB",
+  hasTitle: true
 };
--- docs/stories/components/charts/LRouteChart.stories.js
+++ docs/stories/components/charts/LRouteChart.stories.js
@@ -1,42 +1,42 @@
-import LRouteChart from '@components/charts/LRouteChart.vue';
+import LRouteChart from "@components/charts/LRouteChart.vue";
 
 export default {
-  title: 'Design System/Components/Charts/Flow',
+  title: "Components/Charts/Flow",
   component: LRouteChart,
   argTypes: {
-    header: {control: 'array', description: 'Chart headers array'},
-    lines: { control: 'object', description: 'Chart content object'},
-    title: {table: {disable: true}},
-    description: {table: {disable: true}},
-  },
+    header: { control: "array", description: "Chart headers array" },
+    lines: { control: "object", description: "Chart content object" },
+    title: { table: { disable: true } },
+    description: { table: { disable: true } }
+  }
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { LRouteChart },
-  template: '<l-route-chart v-bind="$props"> </l-route-chart>',
+  template: '<l-route-chart v-bind="$props"> </l-route-chart>'
 });
 
 export const Default = Template.bind({});
 Default.args = {
-    header: ['Origin Port', 'Discharge Port', 'Destiny Port'],
-    lines: [
-      {
-        number: '60%',
-        quantity: {
-          name: 'TEUS',
-          value: 22
-        },
-        values: ['Santos', 'Norfolk', 'Everglades Port']
+  header: ["Origin Port", "Discharge Port", "Destiny Port"],
+  lines: [
+    {
+      number: "60%",
+      quantity: {
+        name: "TEUS",
+        value: 22
       },
-      {
-        number: '20%',
-        values: ['Umuarama', 'Bosque Uirapuru', 'Porto do mané']
-      },
-      {
-        number: '20%',
-        isLast: true,
-        values: 'Others'
-      }
-    ]
+      values: ["Santos", "Norfolk", "Everglades Port"]
+    },
+    {
+      number: "20%",
+      values: ["Umuarama", "Bosque Uirapuru", "Porto do mané"]
+    },
+    {
+      number: "20%",
+      isLast: true,
+      values: "Others"
+    }
+  ]
 };
