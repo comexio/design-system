c33ba9923066e3abba4e5cc5394261c6e061f963
MatheuzsrLog <matheus.rocha@logcomex.com>
2021-01-12 10:24:38 -0300

    Personalizao do storybook by ayla

---
+++ .storybook/ayla/colors.js
@@ -0,0 +1,5 @@
+
+export const primary = '#5C068C'
+export const secondary = '#8345A5'
+export const terciary = '#FAF6FF'
+export const textColorLight = '#E2DAF8'
---
+++ .storybook/ayla/index.js
@@ -0,0 +1,34 @@
+// .storybook/YourTheme.js
+
+import { create } from "@storybook/theming/create";
+
+import { primary, secondary, terciary, textColorLight } from "./colors";
+
+export default create({
+  brandTitle: "Ayla",
+  base: "light",
+
+  colorPrimary: primary,
+  colorSecondary: secondary,
+
+  // UI
+  appBg: terciary,
+
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: "monospace",
+
+  // Text colors
+  textColor: secondary,
+  textInverseColor: "rgba(255,255,255,0.9)",
+
+  // Toolbar default and active colors
+  barTextColor: terciary,
+  barSelectedColor: textColorLight,
+  barBg: primary,
+
+  //Image and link project
+  brandUrl: "https://github.com/comexio/design-system",
+  brandImage:
+    "https://raw.githubusercontent.com/comexio/design-system/master/.github/assets/ayla-logo.png"
+});
