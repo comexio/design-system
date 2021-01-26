9a5de93af61cad28ef2a7a88d68352e8a2b2b7d4
Kauan Schaeffer <kauan.schaeffer@logcomex.com>
2021-01-26 14:46:36 -0300

    (LOG-14486) LModal e LModalContent corrigidos
    - Documentação finalizada
    - Erro do storybook paliativamente corrigido

--- docs/stories/components/modals/LModal.stories.js
+++ docs/stories/components/modals/LModal.stories.js
@@ -1,50 +1,88 @@
-// import LModal from '@components/modal/LModal.vue';
+import vuetify from '~/.storybook/vuetify_storybook.js';     
+import LModal from '@components/modal/LModal.vue';
 
-// export default {
-//   title: 'Design System/Components/Base/Modal/LModal',
-//   component: LModal,
-//   argTypes: {
-//     dialog: { control: 'boolean', description:'Open and close modal open: true / close: false' },
-//     textCancel: { control: 'text', description: 'Text Button Cancel' },
-//     textConfirm: { control: 'text', description: 'Text Button Confirm' },
-//     disabledConfirm: { control: 'boolean', description:'Disable button confirm disable: true / enable: false' },
-//     confirmButton: { control: 'boolean', description:'Show button confirm show: true / show: false' },
-//     clearButton: { table: { disable: true }, description:'N' },
-//     textClear: { control: 'text', description: 'Text button clear' },
-//     clearIcon: { control: 'text', description: 'Icon button clear' },
-//     loading: { control: 'boolean', description: 'Loading modal'},
-//     close: { action: 'close', description: 'Event close modal' },
-//     confirm: { action: 'confirm', description: 'Event confirm modal' },
-//     clear: { action: 'clear', description: 'Event clear modal' },
-//   },
-// };
+export default {
+  title: 'Components/Modals/Modal',
+  component: LModal,
+  argTypes: {
+      modalType: {control: 'object', description: 'Indicates the modalType: confirmational (with action buttons) / informational (with closable button)'},
+      dialog: {control: 'boolean', description: 'Indicates the state of the modal (open/close)'},
+      disabledConfirm: {control: 'boolean', description: 'Indicates if confirm button is disabled or not'},
+      confirmButton: {control: 'boolean', description: 'Indicates if confirm button will be rendered or not'},
+      clearButton: {control: 'boolean', description: 'Indicates if clear button will be rendered or not'},
+      clearIcon: {control: 'text', description: 'Clear icon value (mdi)'},
+      confirmText: {control: 'text', description: 'Confirm button text'},
+      cancelText: {control: 'text', description: 'Cancel button text'},
+      clearText: {control: 'text', description: 'Clear button text'},
+      loading: {control: 'boolean', description: 'Indicates if modal is loading or not'},
+      maxWidth: {control: 'text', description: 'Max width allowed to the modal'},
+      overlayColor: {control: 'color', description: 'Modal overlay color'},
+      close: {action: 'close', description: 'Emitted when modal is closed'},
+      confirm: {action: 'confirm', description: 'Emitted when confirm button is pressed'},
+      clear: {action: 'clear', description: 'Emitted when clear button is pressed'},
+      modalTitle: {description: 'Modal title slot, only avaliable when the type is informational'},
+      modalContent: {description: 'Modal content slot, which receives modal children elements'},
+  },
+};
 
-// const Template = (args, { argTypes }) => ({
-//   props: Object.keys(argTypes),
-//   components: { LModal },
-//   template: '<l-modal v-bind="$props" @close="close" @confirm="confirm" @clear="clear" style="height: 400px" ></l-modal>'
-// });
+const Template = (args, { argTypes }) => ({
+  vuetify,
+  props: Object.keys(argTypes),
+  components: { LModal },
+  template: `
+    <l-modal 
+        v-bind="$props" 
+        @close="close" 
+        @confirm="confirm" 
+        @clear="clear" 
+    >
+        <template #modalTitle>
+            <div style="textAlign: center">
+                Modal title here!
+            </div>
+        </template>
+        <template #modalContent>
+            <div v-for="x in 5" style="textAlign: center;">
+                Modal Content here!
+            </div>
+        </template>
+    </l-modal>
+  `
+});
 
-// export const Default = Template.bind({});
-// Default.args = {
-//   dialog: true,
-//   disabledConfirm:true,
-//   textConfirm: 'Confirm',
-//   textCancel: 'Cancel',
-// };
+export const Confirmational = Template.bind({});
+Confirmational.args = {
+  dialog: true,
+  modalType: {confirmational: true},
+  confirmText: 'Confirm',
+  cancelText: 'Cancel'
+};
 
-// export const Clear = Template.bind({});
-// Clear.args = {
-//   dialog: true,
-//   textClear: 'Button clear',
-//   textConfirm: 'Confirm',
-//   textCancel: 'Cancel',
-// };
+export const WithClearButton = Template.bind({});
+WithClearButton.args = {
+  dialog: true,
+  modalType: {confirmational: true},
+  confirmText: 'Confirm',
+  cancelText: 'Cancel',
+  clearText: 'Clear',
+  clearButton: true
+};
 
-// export const Loading = Template.bind({});
-// Loading.args = {
-//   dialog: true,
-//   textConfirm: 'Confirm',
-//   textCancel: 'Cancel',
-//   loading: true
-// };
+export const Informational = Template.bind({});
+Informational.args = {
+  dialog: true,
+  modalType: {informational: true},
+  confirmText: 'Confirm',
+  cancelText: 'Cancel',
+  clearText: 'Clear',
+  clearButton: true
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  dialog: true,
+  modalType: {confirmational: true},
+  confirmText: 'Confirm',
+  cancelText: 'Cancel',
+  loading: true
+};
--- docs/stories/components/modals/LModalContent.stories.js
+++ docs/stories/components/modals/LModalContent.stories.js
@@ -1,18 +1,21 @@
 
+import vuetify from '~/.storybook/vuetify_storybook.js';     
 import LModalContent from '@components/modal/LModalContent.vue';
 
 export default {
-  title: 'Design System/Components/Base/Modal/LModalContent',
+  title: 'Components/Modals/Modal Content',
   component: LModalContent,
   argTypes: {
     img: { control: 'text', description: 'Image path' },
-    imgAlt: { control: 'text', description: 'Alternative image path' },
-    title: { control: 'text', description: 'Modal title' },
-    subtitle: { control: 'text', description: 'Modal subtitle' },
+    imgAlt: { control: 'text', description: 'Alternative text to image' },
+    title: { control: 'text', description: 'Content title' },
+    subtitle: { control: 'text', description: 'Content subtitle' },
+    default: {table: {disable: true}}
   },
 };
 
 const Template = (args, { argTypes }) => ({
+  vuetify,
   props: Object.keys(argTypes),
   components: { LModalContent },
   template: '<l-modal-content v-bind="$props"> </l-modal-content>',
@@ -20,7 +23,7 @@ const Template = (args, { argTypes }) => ({
 
 export const Default = Template.bind({});
 Default.args = {
-  img: 'https://picsum.photos/200/300',
+  img: 'https://i.imgur.com/bOpU1tD.png',
   imgAlt: 'Illustrative picture',
   title: 'Title Modal',
   subtitle: 'Subtitle Modal'
