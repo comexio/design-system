26e0a7218d538cc813ca38def05e453a33fbf489
matheuzsr <matheusdasilvarocha@gmail.com>
2021-06-17 10:59:38 -0300

    (LOG-29175) Revert na documentacao

--- docs/stories/components/inputs/LDropdownCheckbox.stories.js
+++ docs/stories/components/inputs/LDropdownCheckbox.stories.js
@@ -6,7 +6,6 @@ export default {
   component: LDropdownCheckbox,
   argTypes: {
     items: {control: 'object', description: 'Array of objects containing all items. Items must have label and value'},
-    value: {control: 'array', description: 'Array of checked items'},
     selectedItems: {control: 'array', description: 'Optional array containing values of preselected items'},
     updatedItems: {action: 'updatedItems', description: 'Update selected items'},
   },
@@ -30,16 +29,5 @@ Default.args = {
         {label: 'Item4', value: 4},
       ],
     selectedItems: [1, 3],
-
-};
-
-Default.args = {
-  items: [
-      {label: 'Item1', value: 1},
-      {label: 'Item2', value: 2},
-      {label: 'Item3', value: 3},
-      {label: 'Item4', value: 4},
-    ],
-  'v-model': [1, 3],
 
 };
