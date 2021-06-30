2d56706aa90f78980e4365c7f3f689ea1057a4cd
Rafael <rafaelbastiansch@gmail.com>
2021-06-29 22:55:17 -0300

    add packages to be external

--- vue.config.js
+++ vue.config.js
@@ -8,16 +8,10 @@ module.exports = {
       }
     },
     externals: [
-      'core-js',
       'dayjs',
       'moment',
       'ramda',
-      'sortablejs',
-      'vue',
-      'vue-class-component',
-      'vue-i18n',
-      'vue-property-decorator',
-      'vuetify',
+      'sortablejs'
     ],
   },
   css: {
