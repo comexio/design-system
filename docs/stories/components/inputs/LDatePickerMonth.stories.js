210174bc628c48e010f791f5b15a03097984223b
Kauan Schaeffer <kauan.schaeffer@logcomex.com>
2021-09-03 16:28:11 -0300

    (LOG-34819) Melhoria mecanismo de cores Datepickers

--- docs/stories/components/inputs/LDatePickerMonth.stories.js
+++ docs/stories/components/inputs/LDatePickerMonth.stories.js
@@ -28,7 +28,9 @@ export default {
       control: 'text',
       description: 'Locale of the datepicker'
     },
+    dropdownIcon: { control: 'boolean', description: 'Toggles dropdown icon visibility' },
     bordered: { control: 'boolean', description: 'Toggles datepicker border and adds LDatePickerMonth--bordered class' },
+    borderColor: { control: 'color', description: 'Color of the border' },
     input: {
       table: {
         disable: true
