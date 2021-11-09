d230c27d86e4d2058943137cbc075daaf4267a80
Kauan Schaeffer <kauan.schaeffer@logcomex.com>
2021-11-09 14:23:14 -0300

    (LOG-41633) Add mecanismo customColors LButtonNew

---
+++ enum/buttonColors.enum.ts
@@ -0,0 +1,22 @@
+// TODO: Ajustar mecanismo de usar variáveis sass no script e substituir as strings de hexa pelas variáveis
+export const PRIMARY = {
+  '--backgroundColor': '#FF8F1C',
+  '--backgroundColorOnHover': '#DB660A',
+  '--backgroundColorDisabled': '#FFBF7D',
+  '--color': '#FFFFFF'
+}
+
+export const SECONDARY = {
+  '--borderColor': '#FF8F1C',
+  '--borderColorOnHover': '#DB660A',
+  '--borderColorDisabled': '#FFBF7D',
+  '--color': '#FF8F1C',
+  '--colorOnHover': '#DB660A',
+  '--colorDisabled': '#FFBF7D'
+}
+
+export const TERTIARY = {
+  '--color': '#FF8F1C',
+  '--colorOnHover': '#DB660A',
+  '--colorDisabled': '#FFBF7D'
+}
