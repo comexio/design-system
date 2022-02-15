5836fa50a4842fbdab0cf36b8d8a6ea3b651ab05
Kauan Schaeffer <kauan.schaeffer@logcomex.com>
2022-02-15 12:14:20 -0300

    (LOG-41286) Remoção LModal

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,11 +1,9 @@
-import LModal from './LModal'
 import LModalContent from './LModalContent'
 import LModalNew from './LModalNew'
 import LModalHeaderNew from './LModalHeaderNew'
 import LModalFooterNew from './LModalFooterNew'
 
 export {
-  LModal,
   LModalContent,
   LModalNew,
   LModalHeaderNew,
