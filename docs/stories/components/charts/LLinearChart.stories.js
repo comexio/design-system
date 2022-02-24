4568fe7c06d1f0e2b6d6482e58b2cf468a17f877
jeronimo-rocha <61020566+jeronimo-rocha@users.noreply.github.com>
2022-02-24 15:49:32 -0300

    (LOG-52045) - Adição de propriedade para de lupa para o linearChart #feature #time #comment - Adição de propriedade para de lupa para o linearChart

--- docs/stories/components/charts/LLinearChart.stories.js
+++ docs/stories/components/charts/LLinearChart.stories.js
@@ -22,7 +22,8 @@ export default {
     loadingExpand: { table: {disable: true} },
     generateColor: { table: {disable: true} },
     isTagChart:  {table: {disable: true} },
-    sectionAfterValue: { description: 'Slot can show extra content and return info to parent components to show more details' }
+    sectionAfterValue: { description: 'Slot can show extra content and return info to parent components to show more details' },
+    magnifyItensQuantity: { control: 'boolean', description: 'Add a magnify in elements list (using index to compare if magnify number is lower than), and if clicked active emit linearChartItemDetail. Default is 0 ' }
   },
 };
 
