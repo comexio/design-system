90a055ee5eddd46743beaaf4e45598c16b554857
rafaelbastiansch <rafael.bastiansch@logcomex.com>
2022-06-01 15:16:09 -0300

    (LOG-52113) - Removido componente LModalContent - Duplicado com o LSModalContent no shared

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,10 +1,8 @@
-import LModalContent from './LModalContent'
 import LModalNew from './LModalNew'
 import LModalHeaderNew from './LModalHeaderNew'
 import LModalFooterNew from './LModalFooterNew'
 
 export {
-  LModalContent,
   LModalNew,
   LModalHeaderNew,
   LModalFooterNew
