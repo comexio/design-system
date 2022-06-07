aa9534a7471ed8727608024736db50c18b230e67
rafaelbastiansch <rafael.bastiansch@logcomex.com>
2022-06-07 18:05:08 -0300

    (LOG-59256) - Removido componente LAlert

--- src/components/alerts/index.js
+++ src/components/alerts/index.js
@@ -1,11 +1,9 @@
-import LAlert from './LAlert'
 import LHeaderAlert from './LHeaderAlert'
 import LHeaderAlertContent from './LHeaderAlertContent'
 import LToast from './LToast'
 import LToastContent from './LToastContent'
 
 export {
-  LAlert,
   LHeaderAlert,
   LHeaderAlertContent,
   LToast,
